60fef6e75fd563c91d00b2f56efe7129ac9ca2b6
Complexlity <nwalozielijah@gmail.com>
2024-10-07 08:35:25 +0100

    chore: mock auth

--- components/foundation/AccountButton.tsx
+++ components/foundation/AccountButton.tsx
@@ -1,101 +1,107 @@
-'use client'
+"use client";
 import {
-    AuthKitProvider,
-    SignInButton,
-    type StatusAPIResponse,
-    useProfile,
-} from '@farcaster/auth-kit'
-import '@farcaster/auth-kit/styles.css'
-import BaseSpinner from '@/components/shadcn/BaseSpinner'
+  AuthKitProvider,
+  SignInButton,
+  type StatusAPIResponse,
+  useProfile,
+} from "@farcaster/auth-kit";
+import "@farcaster/auth-kit/styles.css";
+import BaseSpinner from "@/components/shadcn/BaseSpinner";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from '@/components/shadcn/Tooltip'
-import { getCsrfToken, signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useCallback, useState } from 'react'
-import { LogOut as LogOutIcon } from 'react-feather'
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/shadcn/Tooltip";
+import { getCsrfToken, signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { LogOut as LogOutIcon } from "react-feather";
+import { auth } from "@/auth";
 
 export default function AccountButton() {
-    const sesh = useSession()
-    const router = useRouter()
-    const [isLoggingIn, setIsLoggingIn] = useState(false)
-    const [timeSpent, setTimeSpent] = useState(0)
+  // const sesh = useSession()
+  const sesh = auth();
+  sesh.status = "authenticated";
+  const router = useRouter();
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const [timeSpent, setTimeSpent] = useState(0);
 
-    const { isAuthenticated } = useProfile()
+  const { isAuthenticated } = useProfile();
 
-    const getNonce = useCallback(async () => {
-        const nonce = await getCsrfToken()
-        if (!nonce) throw new Error('Unable to generate nonce')
-        return nonce
-    }, [])
+  const getNonce = useCallback(async () => {
+    const nonce = await getCsrfToken();
+    if (!nonce) throw new Error("Unable to generate nonce");
+    return nonce;
+  }, []);
 
-    const increaseTimeSpent: () => ReturnType<typeof setTimeout> | undefined = useCallback(() => {
-        setTimeSpent((prev) => prev + 1)
-        return setTimeout(increaseTimeSpent, 1000)
-    }, [])
+  const increaseTimeSpent: () => ReturnType<typeof setTimeout> | undefined =
+    useCallback(() => {
+      setTimeSpent((prev) => prev + 1);
+      return setTimeout(increaseTimeSpent, 1000);
+    }, []);
 
-    const handleLogin = useCallback(
-        (res: StatusAPIResponse) => {
-            setIsLoggingIn(true)
+  const handleLogin = useCallback(
+    (res: StatusAPIResponse) => {
+      setIsLoggingIn(true);
 
-            const ref = increaseTimeSpent()
+      const ref = increaseTimeSpent();
 
-            signIn('credentials', {
-                message: res.message,
-                signature: res.signature,
-                name: res.username,
-                pfp: res.pfpUrl,
-                redirect: false,
-            }).then((r) => {
-                if (!r?.error) {
-                    router.refresh()
-                }
-            })
+      signIn("credentials", {
+        message: res.message,
+        signature: res.signature,
+        name: res.username,
+        pfp: res.pfpUrl,
+        redirect: false,
+      }).then((r) => {
+        if (!r?.error) {
+          router.refresh();
+        }
+      });
 
-            return () => {
-                if (ref) {
-                    clearTimeout(ref)
-                }
-            }
-        },
-        [router, increaseTimeSpent]
-    )
+      return () => {
+        if (ref) {
+          clearTimeout(ref);
+        }
+      };
+    },
+    [router, increaseTimeSpent]
+  );
 
-    return (
-        <AuthKitProvider>
-            {sesh?.status === 'loading' ? (
-                <div className="w-12 h-12 bg-transparent" />
-            ) : isLoggingIn ? (
-                <div className="flex flex-col justify-center items-center w-full h-full gap-2">
-                    <BaseSpinner />
-                    {timeSpent > 5 && <p className="text-sm">Stuck? Try refreshing the page!</p>}
-                </div>
-            ) : isAuthenticated || sesh?.status === 'authenticated' ? (
-                <TooltipProvider delayDuration={0}>
-                    <Tooltip>
-                        <TooltipTrigger>
-                            <LogOutIcon
-                                size={16}
-                                onClick={() => signOut()}
-                                className="cursor-pointer stroke-white/40"
-                            />
-                        </TooltipTrigger>
-                        <TooltipContent className="max-w-72 mr-8" side="bottom">
-                            <p>Tap to sign out</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-            ) : (
-                <SignInButton
-                    nonce={getNonce}
-                    onSuccess={handleLogin}
-                    onSignOut={() => signOut()}
-                    hideSignOut={true}
-                />
-            )}
-        </AuthKitProvider>
-    )
+  return (
+    <AuthKitProvider>
+      {sesh?.status === "loading" ? (
+        <div className="w-12 h-12 bg-transparent" />
+      ) : isLoggingIn ? (
+        <div className="flex flex-col justify-center items-center w-full h-full gap-2">
+          <BaseSpinner />
+          {timeSpent > 5 && (
+            <p className="text-sm">Stuck? Try refreshing the page!</p>
+          )}
+        </div>
+      ) : isAuthenticated || sesh?.status === "authenticated" ? (
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger>
+              <LogOutIcon
+                size={16}
+                onClick={() => signOut()}
+                className="cursor-pointer stroke-white/40"
+              />
+            </TooltipTrigger>
+            <TooltipContent className="max-w-72 mr-8" side="bottom">
+              <p>Tap to sign out</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        <SignInButton
+          nonce={getNonce}
+          onSuccess={handleLogin}
+          onSignOut={() => signOut()}
+          hideSignOut={true}
+        />
+      )}
+    </AuthKitProvider>
+  );
 }
--- components/home/TemplateCard.tsx
+++ components/home/TemplateCard.tsx
@@ -1,73 +1,82 @@
-'use client'
-import { createFrame } from '@/lib/frame'
-import type { getTemplates } from '@/lib/template'
-import type templates from '@/templates'
-import { useSession } from 'next-auth/react'
-import NextImage from 'next/image'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { Plus as PlusIcon } from 'react-feather'
-import { Separator } from '../shadcn/Separator'
+"use client";
+import { createFrame } from "@/lib/frame";
+import type { getTemplates } from "@/lib/template";
+import type templates from "@/templates";
+import { useSession } from "next-auth/react";
+import NextImage from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Plus as PlusIcon } from "react-feather";
+import { Separator } from "../shadcn/Separator";
+import { auth } from "@/auth";
 
 export default function TemplateCard({
-    template,
+  template,
 }: {
-    template: Awaited<ReturnType<typeof getTemplates>>[number]
+  template: Awaited<ReturnType<typeof getTemplates>>[number];
 }) {
-    const sesh = useSession()
-    const router = useRouter()
+  // const sesh = useSession()
+  const authUser = auth();
+  const sesh = {
+    data: authUser,
+  };
 
-    const [isLoading, setLoading] = useState(false)
+  const router = useRouter();
 
-    const { name, description, creatorName, cover } = template
+  const [isLoading, setLoading] = useState(false);
 
-    async function createAndNavigate() {
-        if (!sesh.data?.user) {
-            document.querySelector('.fc-authkit-signin-button')?.querySelector('button')?.click()
-            return
-        }
-        setLoading(true)
-        const newFrame = await createFrame({
-            name: 'My Frame',
-            template: template.id as keyof typeof templates,
-        })
-        router.push('/frame/' + newFrame.id)
-    }
+  const { name, description, creatorName, cover } = template;
 
-    return (
-        <button
-            type="button"
-            className={`transition ease-in-out group w-[350px] h-[380px] flex flex-col rounded-md border-[#32383E] border hover:border-[#12467B]   ${
-                isLoading ? 'animate-pulse' : ''
-            }`}
-            disabled={isLoading}
-            onClick={createAndNavigate}
-        >
-            <div className="relative w-full h-full">
-                <NextImage
-                    src={cover}
-                    alt={name}
-                    fill={true}
-                    className="object-cover h-64 rounded-t-md"
-                />
-                <div className="ease-in-out bg-[#0B0D0E] inline-flex absolute right-4 -bottom-8 justify-center items-center w-14 h-14 rounded-full bg-background group-hover:bg-red-600 group-hover:text-white transition duration-150">
-                    <PlusIcon size={32} />
-                </div>
-            </div>
+  async function createAndNavigate() {
+    // if (!sesh.data?.user) {
+    //   document
+    //     .querySelector(".fc-authkit-signin-button")
+    //     ?.querySelector("button")
+    //     ?.click();
+    //   return;
+    // }
+    setLoading(true);
+    const newFrame = await createFrame({
+      name: "My Frame",
+      template: template.id as keyof typeof templates,
+    });
+    router.push("/frame/" + newFrame.id);
+  }
 
-            <div className="flex flex-col justify-between w-full h-full">
-                <div className="flex flex-col gap-2 items-start p-4 text-start">
-                    <p className="text-xl font-semibold">{name + ' Template'}</p>
-                    <p className="text-md text-[#cdd7e1]">{description}</p>
-                </div>
+  return (
+    <button
+      type="button"
+      className={`transition ease-in-out group w-[350px] h-[380px] flex flex-col rounded-md border-[#32383E] border hover:border-[#12467B]   ${
+        isLoading ? "animate-pulse" : ""
+      }`}
+      disabled={isLoading}
+      onClick={createAndNavigate}
+    >
+      <div className="relative w-full h-full">
+        <NextImage
+          src={cover}
+          alt={name}
+          fill={true}
+          className="object-cover h-64 rounded-t-md"
+        />
+        <div className="ease-in-out bg-[#0B0D0E] inline-flex absolute right-4 -bottom-8 justify-center items-center w-14 h-14 rounded-full bg-background group-hover:bg-red-600 group-hover:text-white transition duration-150">
+          <PlusIcon size={32} />
+        </div>
+      </div>
 
-                <div className="flex flex-col items-start bg-[#171a1c] rounded-b-md">
-                    <Separator className="bg-[#32383E]" />
-                    <p className="text-xs text-start font-medium text-[#9fa6ad] p-3">
-                        Created by {creatorName}
-                    </p>
-                </div>
-            </div>
-        </button>
-    )
+      <div className="flex flex-col justify-between w-full h-full">
+        <div className="flex flex-col gap-2 items-start p-4 text-start">
+          <p className="text-xl font-semibold">{name + " Template"}</p>
+          <p className="text-md text-[#cdd7e1]">{description}</p>
+        </div>
+
+        <div className="flex flex-col items-start bg-[#171a1c] rounded-b-md">
+          <Separator className="bg-[#32383E]" />
+          <p className="text-xs text-start font-medium text-[#9fa6ad] p-3">
+            Created by {creatorName}
+          </p>
+        </div>
+      </div>
+    </button>
+  );
 }
